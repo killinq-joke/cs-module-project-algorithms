3bcca1d40bc344511385b8ff3ff682760882a17e
zakaria touzri <zakadmaster@gmail.com>
2020-07-22 22:21:37 +0200

    --wip-- [skip ci]

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -4,8 +4,18 @@ Returns: an integer
 '''
 def eating_cookies(n):
     # Your code here
-
-    pass
+    result = 0
+    if n == 0:
+        return 1
+        
+    if n > 3:
+        result += eating_cookies(n - 3)
+    if n > 2:
+        result += eating_cookies(n - 2)
+    if n >= 1:
+        result += eating_cookies(n - 1)
+    
+    return result
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
